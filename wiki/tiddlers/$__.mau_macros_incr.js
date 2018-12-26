7667320826f1079e6d7f0d81ca2eff2bc9bee5d2
mauloop <mauloop@gmail.com>
2018-12-26 12:47:30 +0100

    Consolidate old uncommitted changes

--- wiki/tiddlers/$__.mau_macros_incr.js
+++ wiki/tiddlers/$__.mau_macros_incr.js
@@ -1,10 +1,9 @@
 /*\
-title: $:/plugins/DocTool/macros/incr.js
+title: $:/.mau/macros/incr.js
 type: application/javascript
 module-type: macro
 
-Macro to increment a value by 1
-
+Increments a given value by 1 (or more)
 \*/
 (function(){
 
@@ -19,15 +18,16 @@ Information about this macro
 exports.name = "incr";
 
 exports.params = [
-	{name: "val"}
+    {name: "val"},
+    {name: "add", default: 1}
 ];
 
 /*
 Run the macro
 */
-exports.run = function(val) {
-        var n = Number(val) + 1;
-        return (n);
+exports.run = function(val, add) {
+    var n = Number(val) + Number(add);
+    return (n);
 };
 
 })();
